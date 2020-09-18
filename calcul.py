573bbe2581f1cb1bcc787ee3da9367a28146c834
kramynina <71281030+kramynina@users.noreply.github.com>
2020-09-18 23:29:23 +0800

    Add files via upload

---
+++ calcul.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+def mult(a, b):
+    return ( a * b )
+def summ(a, b):
+    return ( a + b )
+a = int(input('Введите 1 число: '))
+b = int(input('Введите 2 число: '))
+print("Произведение равно")
+print (mult(a, b))
+print("Сумма равна")
+print (summ(a, b))
